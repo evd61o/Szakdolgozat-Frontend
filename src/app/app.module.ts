800ee586cda1d08e55efb7eea2e2d7d85363c5db
szpat <evd61o@hallgato.uniduna.hu>
2023-09-08 17:47:00 +0200

    alap frontend frontend-backend osszekotve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import { KalkulatorComponent } from './pages/kalkulator/kalkulator.component';
+import {ApiService} from "./pages/api/api.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    KalkulatorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/api/api.service.ts
@@ -0,0 +1,34 @@
+import {Inject, Injectable, InjectionToken, Optional} from "@angular/core";
+import {HttpClient, HttpEvent, HttpResponse} from "@angular/common/http";
+import { Observable } from "rxjs";
+
+
+export interface Refrigerator {
+  Marka?: string;
+  Modell?: string;
+  Tipus?: string;
+  Meret?: number;
+  Energiahatekonysagi_osztaly?: string;
+  Fogyasztasev?: number;
+  Fogyasztasnap?: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  protected basePath = 'http://localhost:3000';
+
+  constructor(protected httpClient: HttpClient) {
+  }
+
+  public getRefrigeratorsFromApi$(): Observable<Array<Refrigerator>>;
+  public getRefrigeratorsFromApi$(): Observable<HttpResponse<Array<Refrigerator>>>;
+  public getRefrigeratorsFromApi$(): Observable<HttpEvent<Array<Refrigerator>>>;
+  public getRefrigeratorsFromApi$(): Observable<any> {
+
+    /*this.httpClient.request<Array<Refrigerator>>('get',`${this.basePath}/api/v1/flight-cancellation/${encodeURIComponent(String(id))}`)*/
+    return this.httpClient.request<Array<Refrigerator>>('get',`${this.basePath}/hutok`)
+  }
+}
+
