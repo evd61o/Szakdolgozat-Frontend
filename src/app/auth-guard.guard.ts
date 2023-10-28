ad38046a866eca90b29bfe607c2cc4a55432bd68
szpat <evd61o@hallgato.uniduna.hu>
2023-10-28 18:47:42 +0200

    bejelentkezés email nem megfelelő formátum hiba hozzáadva
    Signed-off-by: szpat <evd61o@hallgato.uniduna.hu>

--- src/app/auth-guard.guard.ts
+++ src/app/auth-guard.guard.ts
@@ -14,13 +14,29 @@ export class AuthGuardGuard implements CanActivate {
   constructor(private readonly router: Router, private readonly loginService: LoginService ) {}
 
   public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
-    return this.getCanLoadResult(this.loginService.getUserEmail());
+    if (state.url === '/bejelentkezes' || state.url === '/regisztracio') {
+      return this.getCanLoadResult(this.loginService.getUserEmail());
+    } else if (state.url === '/kijelentkezes' || state.url === '/profil') {
+      return this.getCanLoadResult2(this.loginService.getUserEmail());
+    }
+    // Alapértelmezett visszatérési érték, ha nincs illeszkedő forgatókönyv
+    return false; // vagy visszairányítás egy UrlTree-vel
   }
 
   private getCanLoadResult(
     userEmail: string
   ): boolean | UrlTree {
     if (!userEmail) {
+      return true;
+    } else {
+      return this.router.createUrlTree([""]);
+    }
+  }
+
+  private getCanLoadResult2(
+    userEmail: string
+  ): boolean | UrlTree {
+    if (userEmail) {
       return true;
     } else {
       return this.router.createUrlTree([""]);
